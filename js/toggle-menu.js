7b15aeebbfa8fb1866da51f652a9d646ddac63f0
Ilya Pashkov <ispashkov@gmail.com>
2018-07-13 12:51:48 +0400

    fix problemki

--- js/toggle-menu.js
+++ js/toggle-menu.js
@@ -5,15 +5,5 @@ const toggleMenu = document.getElementsByClassName("navigation__wrap");
 
 
 toggleButton.addEventListener("click", function () {
-
-    if (nav.className === "nav-toggle") { //if there is a class nav-toggle remove it     
-        nav.classList.remove("nav-toggle");
-        nav.style.display = "none";
-
-    } else {
-        nav.classList.add("nav-toggle");
-        nav.style.display = "block";
-
-    }
-
+		nav.classList.toggle('nav-toggle');
 });
